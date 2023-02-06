d1641ef363fa2ec2997d3181ed7017de9d34ed2f
chshen1998 <chewhoashen@gmail.com>
2023-02-06 16:46:19 +0800

    Add mapper, combiner, reducer

--- TopkCommonWords.java
+++ TopkCommonWords.java
@@ -1,11 +1,83 @@
 /*
-ENTER YOUR NAME HERE
-NAME: JOHN DOE
-MATRICULATION NUMBER: A0123456X
+NAME: CHEW HOA SHEN
+MATRICULATION NUMBER: A0200044E
 */
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class TopkCommonWords {
-    public static void main(String[] args){
 
+    private Set<String> stopwords = new HashSet<String>();
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+        
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+    
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+          StringTokenizer itr = new StringTokenizer(value.toString());
+          while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+          }
+        }
+    }
+
+    public static class IntSumCombiner extends Reducer<Text, IntWritable, Text, IntWritable> {
+        
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+          int sum = 0;
+          for (IntWritable val : values) {
+            sum += val.get();
+          }
+          result.set(sum);
+          context.write(key, result);
+        }
+    }
+
+    public static class IntMinReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+          int minVal = -1;
+          for (IntWritable val : values) {
+            if (minVal == -1 || val < minVal) {
+                minVal = val;
+            } 
+          }
+          result.set(minVal);
+          context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args){
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Top K Common Words");
+        job.setJarByClass(TopkCommonWords.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumCombiner.class);
+        job.setReducerClass(IntMinReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), FileInputFormat.class, TokenizerMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), FileInputFormat.class, TokenizerMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[3]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     } 
 }
